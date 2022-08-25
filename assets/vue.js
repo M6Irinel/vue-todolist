207428c72e2019734f8545630b6a34f2a48a945b
M6Irinel <momoloacairinel666@gmail.com>
2022-08-25 16:21:46 +0200

    first commit

---
+++ assets/vue.js
@@ -0,0 +1,27 @@
+new Vue( {
+    el: '#app',
+    data() {
+        return {
+            todos: [],
+            textTodo: ''
+        }
+    },
+    methods: {
+        addTodo () {
+            this.textTodo = this.textTodo.trim();
+            if ( !this.textTodo ) return;
+            this.todos.push( {
+                text: this.textTodo,
+                done: false
+            } );
+            this.textTodo = '';
+        },
+        done (el_Todo) {
+            el_Todo.done = !el_Todo.done;
+        },
+        enter (e) {
+            if(e.key === 'Enter')
+                this.addTodo();
+        }
+    },
+} );
